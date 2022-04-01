20eafd653dec40c5b8692e2dcb60319e3702511f
CENG 252 <CENG252@gmail.com>
2022-04-01 19:00:34 -0400

    Lab09

--- makefile
+++ makefile
@@ -1,19 +1,21 @@
-vdl: vdl.o logger.o serial.o nmea.o dlgps.o sensehat.o loggermqtt.o
-	g++ -g -o vdl vdl.o logger.o serial.o nmea.o sensehat.o dlgps.o loggermqtt.o -lm -lRTIMULib -lpaho-mqtt3c
-vdl.o: vdl.cpp vdl.h logger.h serial.h nmea.h dlgps.h
+vdl: vdl.o logger.o sensehat.o serial.o nmea.o dlgps.o loggermqtt.o dlfirmata.o
+	g++ -g -o vdl vdl.o logger.o sensehat.o serial.o nmea.o dlgps.o loggermqtt.o dlfirmata.o -lm -lRTIMULib -lpaho-mqtt3c -lboost_thread -lboost_system -lpthread -lopenFrameworksArduinoD
+vdl.o: vdl.cpp vdl.h logger.h sensehat.h serial.h nmea.h dlgps.h
 	g++ -g -c vdl.cpp
-logger.o: logger.cpp logger.h serial.h nmea.h dlgps.h
+logger.o: logger.cpp logger.h sensehat.h serial.h nmea.h dlgps.h loggermqtt.h
 	g++ -g -c logger.cpp
+SenseHat.o: sensehat.cpp sensehat.h
+	g++ -g -c sensehat.cpp
 serial.o: serial.cpp serial.h
 	g++ -g -c serial.cpp
 dlgps.o: dlgps.cpp dlgps.h
 	g++ -g -c dlgps.cpp
 nmea.o: nmea.cpp nmea.h
 	g++ -g -c nmea.cpp
-sensehat.o: sensehat.cpp sensehat.h
-	g++ -g -c sensehat.cpp
 loggermqtt.o: loggermqtt.cpp loggermqtt.h
 	g++ -g -c loggermqtt.cpp
+dlfirmata.o: dlfirmata.cpp dlfirmata.h
+	g++ -g -c dlfirmata.cpp
 clean:
 	touch *
 	rm *.o
--- vdl.cpp
+++ vdl.cpp
@@ -6,10 +6,14 @@
 #include <unistd.h>
 #include "vdl.h"
 #include "logger.h"
+#include <ofArduino.h>
+#include "dlfirmata.h"
+
+ArduinoFirmata ard;
 
 /** @brief Vehicle Data Logger main function
  *  @author Robert Miller
- *  @date 16Jan2022
+ *  @date 30Mar2022
  *  @param void
  *  @return int program status
  */
@@ -20,9 +24,33 @@ int main(void)
   DlInitialization();
 	DlDisplayLogo();
 	sleep(3);
+  if (!ard.connect("ttyACM0", 57600)) { // Serial Port and Baud Rate
+    fprintf(stdout, "\nFailed to connect to arduino!");
+    return -1;
+  }
+  // Need to do this to init the pins, get the firmware version, and  call
+  // setupArduino. Will stay in update loop looking for signal. When it arrives
+  // Setup will be called and we can start processing.
+  ard.sendReset();
+  ard.sendProtocolVersionRequest();
+  ard.sendFirmwareVersionRequest();
+
+  ard.setupArduino(1);
+
   while (1) {
+		ard.update();
+		ard.sendDigital(WHITELED, 1);
     reads = DlGetLoggerReadings();
-		DlUpdateLevel(reads.xa, reads.ya);
+		if (ard.getDigital(LSWITCH) == 1) {
+			ard.sendDigital(BEEPER, 1);
+		} else {
+			ard.sendDigital(REDLED, 1);
+		}
+    usleep(SLEEPTIME);
+    ard.sendDigital(WHITELED, 0);
+    ard.sendDigital(REDLED, 0);
+    ard.sendDigital(BEEPER, 0);
+    DlUpdateLevel(reads.xa, reads.ya);
 		if (tc == LOGCOUNT) {
 			DlDisplayLoggerReadings(reads);
 			DlSaveLoggerData(reads);
